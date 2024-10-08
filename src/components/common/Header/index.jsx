01911bc3cfd1ac99812cd08b6161d6feeac7cf9f
AbbasRostami <abbas.cracker@gmail.com>
2024-10-08 23:12:22 +0330

    str

---
+++ src/components/common/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='head'>Headefjjjj
+    FJ
+    
+    
+    fdsdf
+    dsr Header Header Header</div>
+  )
+}
+
+export { Header };
