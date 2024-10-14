16592e48656f7755eb3c2bbad899e1c219276e8c
AbbasRostami <abbas.cracker@gmail.com>
2024-10-14 22:31:59 +0330

    test-aa

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Landing } from '../screens/Landing'
-import Routes from '../routes'
-import { RouterProvider } from "react-router-dom";
-=======
 import { Landing } from "../screens/Landing";
 import "./App.css";
->>>>>>> dcea426808fb559cc598e8e04a51fd76a936d8e8
 
 function App() {
   // vv
