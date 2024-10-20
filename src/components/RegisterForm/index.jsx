f1d3c4fa9c94749c224478c2c1d01a6dc7737c96
amin <aminbakhshandee0898@gmail.com>
2024-10-20 17:25:50 +0330

    .

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { RegisterStepOne } from "./RegisterStepOne.jsx"
 import { RegisterStepTwo } from './RegisterStepTwo.jsx'
-import { RegisterStepThree } from './RegisterStepThree.jsx'
-import { Header } from "./../common/Header"
-import { Footer } from "./../common/Footer"
+import { RegisterStepTwo } from './RegisterStepThree.jsx'
 
 const RegisterForm = () => {
   return (
     <div>
-                <RegisterStepTwo />
-                <RegisterStepOne />
-                <RegisterStepThree />
-            <Footer />
+            <RegisterStepTwo />
+            <RegisterStepOne />
+            <RegisterStepThree />
     </div>
   )
 }
