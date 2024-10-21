dc237d8fc5b29026144492e2d58bd13423914701
amin <aminbakhshandee0898@gmail.com>
2024-10-21 21:32:35 +0330

    .

--- src/components/AboutForm/index.jsx
+++ src/components/AboutForm/index.jsx
@@ -1,81 +1,111 @@
-import React from "react";
+import React from 'react'
+import { Header } from '../common/Header'
+import { Footer } from '../common/Footer/Footer'
 import { Formik, Field, Form } from "formik";
-import Email from "./../../assets/svg/aboutUs/IconEmail.svg";
-import Phone from "./../../assets/svg/aboutUs/IconPhone.svg";
-import Address from "./../../assets/svg/aboutUs/IconAddress.svg";
+
 
 const AboutForm = () => {
   return (
-    <>
-   
-    <div className="from-emerald-300/85 from-35% to-white  ">
-      
-      </div>
-      <div className="flex justify-around items-center px-28 ">
-        <div>
-          <Formik>
-            <Form>
-              <div
-                className=" flex flex-col items-center mt-10 w-[33rem] h-[32rem] relative rounded-[1.5rem] dark:bg-gray-800
-                                                          shadow-[9px_9px_12px_3px_rgba(0,_0,_0,_0.1)] bg-[#FBF6F6]"
-              >
-                <Field
-                  className="bg-[#FFFFFF] dark:bg-gray-700 dark:text-white rtl mt-10 placeholder-[#807A7A] h-[50px] w-[450px] border-solid border border-TextGreen rounded-md "
-                  placeholder="  نام و نام خانواگی"
-                ></Field>
+    <div>
 
-                <Field
-                  className="bg-[#FFFFFF] dark:bg-gray-700 dark:text-white rtl mt-5 placeholder-[#807A7A]  h-[50px] w-[450px] border-solid border border-TextGreen rounded-md"
-                  placeholder="  ایمیل آدرس"
-                ></Field>
-
-                <Field
-                  className="bg-[#FFFFFF] dark:bg-gray-700 dark:text-white rtl my-5 placeholder-[#807A7A]  h-[50px] w-[450px] border-solid border border-TextGreen rounded-md"
-                  placeholder="  شماره تماس"
-                ></Field>
-
-                <Field
-                  className="bg-[#FFFFFF] dark:bg-gray-700 dark:text-white rtl placeholder-[#807A7A] items-start h-[160px] w-[450px] border-solid border border-TextGreen rounded-md "
-                  placeholder="  متن پیام..."
-                ></Field>
-
-                <button
-                  class="w-[120px] h-[40px] dark:bg-gray-700
-                                     text-white bg-BgGreen rounded-lg absolute right-[2.5rem] bottom-10"
-                >
-                  ارسال
-                </button>
-              </div>
-            </Form>
-          </Formik>
-        </div>
+      <Header />
 
 
 
-        <div class="flex flex-col items-center w-[26rem] ">
-          
+
+
+
+      <div className='flex justify-around items-center'>
+
           <div>
-          <p className="mr-5 rtl text-TextGray text-2xl dark:text-white">با ما در ارتباط باشید</p>
-          <p className="mr-5 mt-3 rtl text-TextGray dark:text-white">
-            لورم ایپسوم بسیار فراگیر است زیرا بسیار همه کاره است. تعداد
-            پاراگراف‌هایی را که می‌خواهید انتخاب کنید، کپی کنید
-          </p>
+
+          <Formik>
+                   <Form >
+                                <div className=' flex flex-col items-center mt-10 w-[37rem] h-[30rem] relative rounded-[1.5rem]
+                                                          shadow-[9px_9px_12px_3px_rgba(0,_0,_0,_0.1)] bg-[#FBF6F6]'> 
+
+                                    <Field className="bg-[#FFFFFF] rtl mt-10 placeholder-[#807A7A] h-[50px] w-[450px] border-solid border border-TextGreen rounded-md "
+                                        placeholder="  نام و نام خانواگی"
+                                    >
+                                    </Field>
+
+                                    <Field className="bg-[#FFFFFF] rtl mt-5 placeholder-[#807A7A]  h-[50px] w-[450px] border-solid border border-TextGreen rounded-md"
+                                        placeholder="  ایمیل آدرس"
+                                    >
+                                    </Field>
+
+                                    <Field className="bg-[#FFFFFF] rtl my-5 placeholder-[#807A7A]  h-[50px] w-[450px] border-solid border border-TextGreen rounded-md"
+                                        placeholder="  شماره تماس"
+                                    >
+                                    </Field>
+
+                                    <Field className="bg-[#FFFFFF] rtl placeholder-[#807A7A] items-start h-[160px] w-[450px] border-solid border border-TextGreen rounded-md "
+                                        placeholder="  متن..."
+                                    >
+                                    </Field>
+
+                                    <button class="w-[120px] h-[40px]
+                                     text-[#21394B] bg-BgGreen rounded-lg absolute right-[3rem] bottom-4">ارسال</button>
+
+                                </div>
+                   </Form>
+                </Formik>
           </div>
-        
-              <img className="mt-16 " src={Address} alt="" />
-              <img className="ml-[8rem] my-4" src={Phone} alt="" />
-              <img className="ml-[8rem]" src={Email} alt="" />
-        
 
-             </div>
+vv
 
+
+          <div>
+
+          </div>
 
 
       </div>
 
-      
-    </>
-  );
-};
 
-export { AboutForm };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <Footer />
+
+    </div>
+  )
+}
+
+export { AboutForm }
