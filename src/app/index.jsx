c0ed1d94e5962709f332b6de43a5d49d9141c19d
AbbasRostami <abbas.cracker@gmail.com>
2024-10-24 21:51:01 +0330

    progress scroll bar added

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,3 +1,4 @@
+import { ProgressBar } from "../components/common/ProgressBar";
 import { RoutesApp } from "../routes";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
@@ -5,6 +6,7 @@ import { RouterProvider } from "react-router-dom";
 function App() {
   return (
     <>
+    <ProgressBar />
       <RouterProvider router={RoutesApp} />
     </>
   );
---
+++ src/components/common/ProgressBar.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+const ProgressBar = () => {
+  const [scrollPercent, setscrollPercent] = useState(20);
+  
+  useEffect(() => {
+
+
+    const handleScroll = () => {
+
+        const winHeight = window.innerHeight;
+        const docuHeight = document.documentElement.scrollHeight;
+        const scrollY = window.scrollY;
+    
+        const scrollPercent = (scrollY / (docuHeight - winHeight)) * 100
+        setscrollPercent(scrollPercent)
+        console.log(scrollPercent);
+        
+    }
+    
+
+    window.addEventListener("scroll", handleScroll)
+    window.removeEventListener
+    return () => {
+        window.removeEventListener("scroll", handleScroll)
+    }
+
+  },[])
+
+  return (
+    <>
+      <div
+        id="progress-container"
+        style={{
+            height: "6px",
+            width: "100%",
+            
+            position: "sticky",
+            top: "0",
+            left: "0",
+            right: "0",
+        }}
+      >
+        <div
+          className="progress-fill"
+          style={{height:"100%", backgroundColor: "#097969	", width:`${scrollPercent}%`}}
+        ></div>
+      </div>
+    </>
+  );
+};
+
+export { ProgressBar };
