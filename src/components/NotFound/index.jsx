22c5e65e289f2ce5e13acca737d68f222ab9f970
AbbasRostami <abbas.cracker@gmail.com>
2024-10-28 19:45:05 +0330

    added link to home

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import notFound from "../../assets/notFound.svg"
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
     <div className='flex flex-col items-center'>
       <img className='w-[72rem] ml-32 h-[35rem] mt-10' src={notFound} alt="" />
-      <button className='bg-[#158B68] mt-5 w-[13rem] h-[3.6rem] my-[-3rem] rounded-xl text-[#E8F2F8] text-2xl text-bold'> بازگشت به خانه</button>
+     <Link to="/">  <button className='bg-[#158B68] mt-5 w-[13rem] h-[3.6rem] my-[-3rem] rounded-xl text-[#E8F2F8] text-2xl text-bold'> بازگشت به خانه</button></Link>
     </div>
   )
 }
